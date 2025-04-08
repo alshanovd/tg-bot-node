e8ecb3ea8e9898d6e1171d9f5a0f7f85d29d6b31
Danil Alshanov <danila-18@mail.ru>
2025-04-09 01:10:29 +1000

    remove express, add tg bot, vercel deploy

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,28 @@
-const express = require("express");
-const app = express();
+import TelegramBot from "node-telegram-bot-api";
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const token = process.env.BOT_TOKEN;
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+const bot = new TelegramBot(token, {
+  polling: true,
+});
 
-module.exports = app;
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message 123123" + msg.text);
+});
