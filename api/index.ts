09d59759127a34e6000f7ed0b052daf75ae6177f
Danil Alshanov <danila-18@mail.ru>
2025-04-09 14:09:34 +1000

    finally working bot

--- api/index.ts
+++ api/index.ts
@@ -2,17 +2,13 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const webhook = {};
+const webhook: Telegraf.LaunchOptions["webhook"] = {
+  domain: "https://tg-bot-node.vercel.app/",
+  port: 80,
+};
 
 bot.on(message("text"), (ctx) => ctx.reply("Hello"));
 
-// Start webhook via launch method (preferred)
 bot.launch({
-  webhook: {
-    // Public domain for webhook; e.g.: example.com
-    domain: "https://tg-bot-node.vercel.app/",
-
-    // Port to listen on; e.g.: 8080
-    port: 80,
-  },
+  webhook,
 });
