697d5de2332d8ef5125d79bce878c2c71a9cc73b
Danil Alshanov <danila-18@mail.ru>
2025-04-10 14:00:15 +1000

    pure response

--- api/index.ts
+++ api/index.ts
@@ -8,16 +8,16 @@ const webhook: Telegraf.LaunchOptions["webhook"] = {
 };
 
 bot.on(message("text"), async ({ reply }) => {
-  await reply("Hello! Here are your buttons");
-  return await reply(
-    "some text",
-    Markup.keyboard([
-      ["button1", "button2"],
-      ["button3", "button4", "button5"],
-    ])
-      .oneTime()
-      .resize()
-  );
+  return await reply("Hello! Here are your buttons");
+  // return await reply(
+  //   "some text",
+  //   Markup.keyboard([
+  //     ["button1", "button2"],
+  //     ["button3", "button4", "button5"],
+  //   ])
+  //     .oneTime()
+  //     .resize()
+  // );
 });
 
 // bot.use(Telegraf.log());
