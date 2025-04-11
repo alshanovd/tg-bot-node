414dc9d99d04659c1df7ee1885a3bbc6341948ac
Danil Alshanov <danila-18@mail.ru>
2025-04-11 15:36:31 +1000

    slice error message

--- api/index.ts
+++ api/index.ts
@@ -54,9 +54,12 @@ bot.on(message("text"), async (ctx) => {
     await ctx.reply(
       "Ошибка выдачи ключа. Либо авторизация кончилась, либо порт был уже занят, либо что то еще. Попробуй снова ввести пин-код."
     );
-    await ctx.reply("```json\n" + JSON.stringify(e) + "\n```", {
-      parse_mode: "Markdown",
-    });
+    await ctx.reply(
+      "```json\n" + JSON.stringify(e).slice(0, 30) + " ... \n```",
+      {
+        parse_mode: "Markdown",
+      }
+    );
   }
 });
 
